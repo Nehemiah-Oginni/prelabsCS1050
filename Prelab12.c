abec2e7dde32dffee5c1a5ab523af6b92176942d
Nehemiah-Oginni <neheoginni2@gmail.com>
2024-11-04 18:04:36 -0600

    start of prelab12

---
+++ Prelab12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void printStudentInfo(Student student);
+void addStudent(Student students[], int *count, Student newStudent);
+void printStudentsGPAGreaterThan(Student *students, int count, float greaterGPA);
+void findStudentWithMinGPA(Student students[], int count);
+void findStudentWithMaxGPA(Student students[], int count);
+void findStudentsByName(Student *students, int count, const char *name);
+
+int main (void)
+{
+    
+}
