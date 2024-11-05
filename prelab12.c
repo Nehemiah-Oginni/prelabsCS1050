ff7ab71a4bcfdc274c4cb10b91fff0de3a4ed99e
Nehemiah <neheoginni2@gmail.com>
2024-11-05 14:36:19 -0600

    small progress using enum and structure. simply beginning to organize.

--- prelab12.c
+++ prelab12.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
+enum StudentYearType
+{
+    freshman = 1,
+    sophmore = 2,
+    junior = 3,
+    senior = 4,
+};
+typedef struct
+{
+    char firstname[20];
+    char lastname[20];
+    enum StudentYearType year;
+    float gpa;
+} Student;
+
 void printStudentInfo(Student student);
 void addStudent(Student students[], int *count, Student newStudent);
 void printStudentsGPAGreaterThan(Student *students, int count, float greaterGPA);
@@ -9,6 +24,10 @@ void findStudentWithMaxGPA(Student students[], int count);
 void findStudentsByName(Student *students, int count, const char *name);
 
 int main (void)
+{
+    
+}
+void printStudentInfo(Student student)
 {
     
 }
